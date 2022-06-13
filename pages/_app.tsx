deff0746ffb78ccbd9a5f7b57efe7fcddd5f557f
Jonatan <84264740+Jontes-Tech@users.noreply.github.com>
2022-06-13 14:06:21 +0200

    This should fix favicons.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Add the favicon */}
+      <Head>
+        <link rel="shortcut icon" href="/public/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png">
+        <link rel="manifest" href="/public/site.webmanifest">
+      </Head>
+      {/* Add the favicon */}
+      {/* Note that the path doesn't include "public" */}
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp
